fea84376a7f7432d8a040064d0274e2d15671f4b
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ CS/DockingMVVM.SL/MainPage.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+
+namespace DockingMVVM.SL {
+    public partial class MainPage : UserControl {
+
+        DockLayoutManagerViewModel dockLayoutManagerViewModel;
+
+        public MainPage() {
+            InitializeComponent();
+            dockLayoutManagerViewModel = new DockLayoutManagerViewModel();
+            DataContext = dockLayoutManagerViewModel;
+        }
+        
+
+        private void AddPanel_Click(object sender, RoutedEventArgs e) {
+            PanelViewModel panelViewModel1 = new PanelViewModel();
+            panelViewModel1.Content = "Panel View Model";
+            panelViewModel1.DisplayName = "Panel View Model";
+            dockLayoutManagerViewModel.Workspaces.Add(panelViewModel1);
+        }
+
+        private void AddDocument_Click(object sender, RoutedEventArgs e) {
+            DocumentViewModel documentViewModel1 = new DocumentViewModel();
+            documentViewModel1.Content = "Document View Model";
+            documentViewModel1.DisplayName = "Document View Model";
+            dockLayoutManagerViewModel.Workspaces.Add(documentViewModel1);
+        }
+    }
+}
---
+++ VB/DockingMVVM.SL/MainPage.xaml.vb
@@ -0,0 +1,40 @@
+﻿Imports System
+Imports System.Collections.Generic
+Imports System.Linq
+Imports System.Net
+Imports System.Windows
+Imports System.Windows.Controls
+Imports System.Windows.Documents
+Imports System.Windows.Input
+Imports System.Windows.Media
+Imports System.Windows.Media.Animation
+Imports System.Windows.Shapes
+
+Namespace DockingMVVM.SL
+    Partial Public Class MainPage
+        Inherits UserControl
+
+        Private dockLayoutManagerViewModel As DockLayoutManagerViewModel
+
+        Public Sub New()
+            InitializeComponent()
+            dockLayoutManagerViewModel = New DockLayoutManagerViewModel()
+            DataContext = dockLayoutManagerViewModel
+        End Sub
+
+
+        Private Sub AddPanel_Click(ByVal sender As Object, ByVal e As RoutedEventArgs)
+            Dim panelViewModel1 As New PanelViewModel()
+            panelViewModel1.Content = "Panel View Model"
+            panelViewModel1.DisplayName = "Panel View Model"
+            dockLayoutManagerViewModel.Workspaces.Add(panelViewModel1)
+        End Sub
+
+        Private Sub AddDocument_Click(ByVal sender As Object, ByVal e As RoutedEventArgs)
+            Dim documentViewModel1 As New DocumentViewModel()
+            documentViewModel1.Content = "Document View Model"
+            documentViewModel1.DisplayName = "Document View Model"
+            dockLayoutManagerViewModel.Workspaces.Add(documentViewModel1)
+        End Sub
+    End Class
+End Namespace
